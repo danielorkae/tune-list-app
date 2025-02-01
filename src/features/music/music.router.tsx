e5716a8c7de37cf8929b018c5a71c7f343a95465
Daniel Lopes <danielorkae@gmail.com>
2025-02-01 16:27:21 -0300

    feat: add routing for music feature

---
+++ src/features/music/music.router.tsx
@@ -0,0 +1,10 @@
+import { Route, Routes } from "react-router-dom";
+import { TopChartPage } from "./pages/top-chart.page";
+
+export function MusicRouter() {
+  return (
+    <Routes>
+      <Route path="/top" element={<TopChartPage />} />
+    </Routes>
+  );
+}
---
+++ src/features/music/pages/top-chart.page.tsx
@@ -0,0 +1,8 @@
+export function TopChartPage() {
+  return (
+    <div>
+      <h1>Top Chart</h1>
+      <p>Here is the top chart page</p>
+    </div>
+  );
+}
